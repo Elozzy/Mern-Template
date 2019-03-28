bfd604996ad9b737f61937a13c51ed1fce2aee2c
Elozzy <st.eloghosa@gmail.com>
2019-03-28 06:38:13 +0100

    added the route

--- server.js
+++ server.js
@@ -2,16 +2,19 @@
 const express = require('express');
 const app = express();
 
+const routes = require('./routes');
+
 const PORT = process.env.PORT || 5000;
+
+// require db connection
+require('./models');
 
 // configure body parser for AJAX requests
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-// routes
-app.get('/', (req, res) => {
-	res.send('Hello from MERN.');
-});
+// used routes
+app.use(routes);
 
 // Bootstrap server
 app.listen(PORT, () => {
