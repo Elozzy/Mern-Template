9ace108b1cf58723fc5698891b58424bea12caef
Elozzy <st.eloghosa@gmail.com>
2019-03-28 05:58:11 +0100

    defined the various methods

--- controllers/moviesControllers.js
+++ controllers/moviesControllers.js
@@ -1,1 +1,33 @@
-const
+const Movie = require('../models/movies');
+
+// Defining all methods and business logic for routes
+
+module.exports = {
+    findAll: function(req, res) {
+        Movie.find(req.query)
+            .then(movies => res.json(movies))
+            .catch(err => res.status(422).json(err));
+    },
+    findById: function(req, res) {
+        Movie.findById(req.params.id)
+            .then(movie => res.json(movie))
+            .catch(err => res.status(422).json(err));
+    },
+    create: function(req, res) {
+        Movie.create(req.body)
+            .then(newMovie => res.json(newMovie))
+            .catch(err => res.status(422).json(err));
+    },
+    update: function(req, res) {
+        Movie.findOneAndUpdate({_id: req.params.id}, req.body)
+            .then(book => res.json(book))
+            .catch(err => res.status(422).json(err));
+    },
+
+    remove: function(req, res) {
+        Movie.findById({_id: req.params.id})
+            .then(movie => movie.remove())
+            .then(allmovies => res.json(allmovies))
+            .catch(err => res.status(422).json(err));
+    }
+};
